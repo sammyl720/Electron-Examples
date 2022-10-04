40c7f7d9cafccd52e385c78afd33a0c167eb4102
sam <sam.leider@hotmail.com>
2022-10-03 23:12:11 -0400

    Add dark mode example

--- renderer.js
+++ renderer.js
@@ -1,9 +1,12 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+const toggleDarkModeBtn = document.getElementById('toggle-dark-mode');
+const resetToSystemBtn = document.getElementById('reset-to-system');
+const themeSourceOutput = document.getElementById('theme-source')
 
-const func = async () => {
-  const response = await versions.ping();
-  console.log(response);
-}
-
-func()
+toggleDarkModeBtn.addEventListener('click', async (e) => {
+  const isDarkMode = await darkMode.toggle();
+  themeSourceOutput.innerHTML = isDarkMode ? 'Dark' : 'Light';
+})
+resetToSystemBtn.addEventListener('click', async (e) => {
+  await darkMode.system();
+  themeSourceOutput.innerHTML = 'System'
+})
